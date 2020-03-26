553885a8e2abbf0aaa9b5b29692878c353480f66
Benicio <benicio.daniel@gmail.com>
2020-03-26 16:31:31 -0300

    Salva dado do login em localstorage

--- frontend/src/pages/Logon/Index.js
+++ frontend/src/pages/Logon/Index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi'; 
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,14 +10,42 @@ import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon () {
+  const [id, setId] = useState('')
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('session', { id })
+      
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert("Falha no Login!")
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero"/>
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="Sua ID"/>
-          <button type="submit" className="button">Entrar</button>
+          <input 
+            type="text" 
+            placeholder="Sua ID"
+            value={id}
+            onChange={event => setId(event.target.value)}
+          />
+          <button 
+            type="submit" 
+            className="button"            
+          >
+              Entrar
+          </button>
           <Link to="/register" className="back-link">
             <FiLogIn size={16} color="#E02041"/>
             Não tenho cadastro
